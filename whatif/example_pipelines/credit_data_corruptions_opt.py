4f78fdbee93af33bef36463f2018a1988489e787
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-02-21 18:22:13 +0100

    Started with credit corruptions

--- whatif/example_pipelines/credit_data_corruptions_opt.py
+++ whatif/example_pipelines/credit_data_corruptions_opt.py
@@ -3,14 +3,25 @@ import numpy as np
 
 from sklearn.linear_model import SGDClassifier
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, FunctionTransformer, OneHotEncoder, label_binarize
 from sklearn.compose import ColumnTransformer
 
+from whatif.example_pipelines.credit_data_corruptions_naive import compute_fairness_metric
 from whatif.utils.utils import get_project_root
 
 
-def execute_credit_pipeline():
+def execute_credit_pipeline_opt(debug):
+    iteration_results = {
+        "test_corruption": [],
+        "train_corruption": [],
+        "corruption_fraction": [],
+        "feature": [],
+        "accuracy": [],
+        "non_protected_fnr": [],
+        "protected_fnr": []
+    }
     def load_train_and_test_data(adult_train_location, adult_test_location, excluded_employment_types):
 
         columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation',
@@ -52,7 +63,7 @@ def execute_credit_pipeline():
 
 
     def extract_labels(adult_train, adult_test):
-        adult_train_labels = label_binarize(adult_train['income-per-year'], classes=['<=50K', '>50K'])
+        adult_train_labels = label_binarize(adult_train['income-per-year'], classes=['<=50K', '>50K']).ravel()
         # The test data has a dot in the class names for some reason...
         adult_test_labels = label_binarize(adult_test['income-per-year'], classes=['<=50K.', '>50K.'])
 
@@ -80,6 +91,29 @@ def execute_credit_pipeline():
 
     model = pipeline.fit(train, train_labels)
 
-    score = model.score(test, test_labels)
+    test_predict = model.predict(test)
+    scores = {}
+    scores['accuracy'] = accuracy_score(test_labels, test_predict)
+    scores['non_protected_fnr'], scores['protected_fnr'] = compute_fairness_metric("race", "White", test, test_labels,
+                                                                                   test_predict)
+    if debug is True:
+        print(f'Accuracy on the test set: {scores["accuracy"]}')
+        print(f'non_protected_fnr on the test set: {scores["non_protected_fnr"]}')
+        print(f'protected_fnr on the test set: {scores["protected_fnr"]}')
 
-    print("Model accuracy on held-out data", score)
+    iteration_results["test_corruption"].append(False)
+    iteration_results["train_corruption"].append(False)
+    iteration_results["corruption_fraction"].append(None)
+    iteration_results["feature"].append(None)
+    iteration_results["accuracy"].append(scores["accuracy"])
+    iteration_results["non_protected_fnr"].append(scores["non_protected_fnr"])
+    iteration_results["protected_fnr"].append(scores["protected_fnr"])
+
+
+def do_credit_corruption_opt(debug):
+    # TODO: Make this general enough for the last 2 params to work? Prbbly not necessary
+    iteration_results = execute_credit_pipeline_opt(debug)
+    return pd.DataFrame(iteration_results)
+
+
+do_credit_corruption_opt(debug=True)
